8324e78a40d630259e22b92712bf4736a4f1fa0e
PavaneeshwarReddy <PavaneeshwarReddy>
2024-07-16 11:48:22 +0530

    After adding git ignore

--- routes/commentsrouter.js
+++ routes/commentsrouter.js
@@ -1,17 +1,17 @@
-const express  = require("express");
+const express = require("express");
 const blogModel = require("../models/PostModel");
 const CommentRouter = express.Router();
 
-CommentRouter.post("/postcomment:id",(request,response)=>{
+CommentRouter.post("/postcomment:id", (request, response) => {
     const blogId = request.params.id;
-    const {content,author} = express.request.body;
+    const { content, author } = express.request.body;
     try {
-        const commentsData = blogModel.find({"_id":blogId});
-        const updatedData = blogModel.update({"_id":blogId},{comments:[...commentsData,{content,author}]});
-        response.status(200).json({"Message":"commented"});
-    } 
-    catch(error) {
-        response.status(500).json({"message":"unable to process request",error});
+        const commentsData = blogModel.find({ "_id": blogId });
+        const updatedData = blogModel.update({ "_id": blogId }, { comments: [...commentsData, { content, author }] });
+        response.status(200).json({ "Message": "commented" });
+    }
+    catch (error) {
+        response.status(500).json({ "message": "unable to process request", error });
     }
 })
 
